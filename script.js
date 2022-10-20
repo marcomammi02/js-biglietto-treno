7e3aa5ff30c48eb8475332b44f37e768ecca537f
marcomammi02 <marcomammi2002@gmail.com>
2022-10-20 20:54:26 +0200

    Adding imputs

---
+++ script.js
@@ -0,0 +1,5 @@
+var km = parseInt(prompt("Quanto è lungo il tuo viaggio? (In km)"))
+var anni = parseInt(prompt("Quanti anni hai?"))
+var tariffaBase = km * 0.21
+console.log(km + " km")
+console.log(anni + " anni")
