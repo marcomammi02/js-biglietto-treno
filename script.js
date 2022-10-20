ac19604b696904a4be756514b95c81add2978cdf
marcomammi02 <marcomammi2002@gmail.com>
2022-10-20 21:22:55 +0200

    Update last version

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var km = parseInt(prompt("Quanto è lungo il tuo viaggio? (In km)"))
-var anni = parseInt(prompt("Quanti anni hai?"))
+var km = parseFloat(prompt("Quanto è lungo il tuo viaggio? (In km)"))
+var anni = parseFloat(prompt("Quanti anni hai?"))
 var tariffaBase = km * 0.21
 console.log(km + " km")
 console.log(anni + " anni")
+if ((anni > 17) && (anni < 65)) {
+    console.log(tariffaBase.toFixed(2) + " euro");
+}
+else if(anni < 18) {
+    var tariffaMinorenne = tariffaBase - ((tariffaBase / 100) * 20) ;
+    console.log(tariffaMinorenne.toFixed(2) + " euro")
+}
+else if(anni > 64) {
+    var tariffaPensionato = tariffaBase - ((tariffaBase / 100) * 40) ;
+    console.log(tariffaPensionato.toFixed(2) + " euro")
+}
